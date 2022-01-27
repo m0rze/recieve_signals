3266649d98704a8efeca6038ab8155f675d893cc
m0rze <40863104+m0rze@users.noreply.github.com>
2022-01-27 15:23:20 +0300

    Update TelegramAccounts.php

--- TelegramAccounts.php
+++ TelegramAccounts.php
@@ -5,8 +5,8 @@ namespace Root;
 class TelegramAccounts
 {
     static $accounts = [
-      ["5024495966:AAFSbe_4i2rFfgs_TERHVSlaq3MkJiwitPY", "-1001677189167"]
+      ["", ""]
     ];
 
-    static $ordersAccount = ["5024495966:AAFSbe_4i2rFfgs_TERHVSlaq3MkJiwitPY", "-1001704999732"];
+    static $ordersAccount = ["", ""];
 }
