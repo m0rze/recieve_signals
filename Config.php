b4664d0c6e0e562401bd560426fb63ad8b8fe151
m0rze <40863104+m0rze@users.noreply.github.com>
2022-01-27 15:22:05 +0300

    Update Config.php

--- Config.php
+++ Config.php
@@ -6,8 +6,8 @@ class Config
 {
     static $phpAlias = "php";
 
-    static $dbName = "admin_signals";
-    static $dbUser = "admin_gfhtrtre";
-    static $dbPass = "wuaWTOU2Td";
+    static $dbName = "";
+    static $dbUser = "";
+    static $dbPass = "";
 
 }
